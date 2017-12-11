26a00db736fda78f3d8640589334c3a6ea71e3ae
Sam Thomson <samt@samt.st>
2017-12-11 13:38:49 +0700

    auth config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
+import firebase from 'firebase'
+import { Header } from './components/common'
 
 class App extends Component {
+
+    componentWillMount() {
+        // Initialize Firebase
+        var config = {
+            apiKey: "AIzaSyCTN56CxAlUOwTTXbYJNS2nMM1Gcyvzfjg",
+            authDomain: "react-native-auth-25e83.firebaseapp.com",
+            databaseURL: "https://react-native-auth-25e83.firebaseio.com",
+            projectId: "react-native-auth-25e83",
+            storageBucket: "react-native-auth-25e83.appspot.com",
+            messagingSenderId: "1092612319986"
+        };
+        firebase.initializeApp(config);
+    }
+
     render () {
         return (
             <View>
+                <Header headerText="Authentication"></Header>
                 <Text>App.js</Text>
             </View>
         )
